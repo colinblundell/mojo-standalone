2f7a329e3dd3b02fc004e9b5842004ffd97b0c6f
Colin Blundell <blundell@chromium.org>
2014-11-03 12:17:25 +0100

    Unbundle gn

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-function install_build_dep {
+ROOT_DIR="$(dirname $(realpath $(dirname "${BASH_SOURCE[0]}")))"
+
+function install_dep_from_tarfile {
   SRC_URL=$1
   PACKAGE_DIR=$2
-  ROOT_DIR="$(dirname $(realpath $(dirname "${BASH_SOURCE[0]}")))"
   FILENAME="$(basename $SRC_URL)"
 
   BUILD_DIR="$(basename $FILENAME .tar.gz)"
@@ -28,10 +29,15 @@ function install_build_dep {
 
 }
 
+# BODY
+
+# Install gn.
+~/depot_tools/download_from_google_storage --bucket chromium-gn --output $ROOT_DIR/buildtools/gn/linux64/gn 56e78e1927e12e5c122631b7f5a46768e527f1d2
+
 # Download and extract PLY
 # Homepage:
 # http://dabeaz.com/ply
-install_build_dep "http://dabeaz.com/ply/ply-3.4.tar.gz" "ply"
+install_dep_from_tarfile "http://dabeaz.com/ply/ply-3.4.tar.gz" "ply"
 
 # Download and extract Jinja2
 # Homepage:
@@ -42,4 +48,4 @@ install_build_dep "http://dabeaz.com/ply/ply-3.4.tar.gz" "ply"
 # https://pypi.python.org/pypi/Jinja2
 JINJA2_SRC_URL="https://pypi.python.org/packages/source/"
 JINJA2_SRC_URL+="J/Jinja2/Jinja2-2.7.1.tar.gz"
-install_build_dep $JINJA2_SRC_URL 'jinja2'
+install_dep_from_tarfile $JINJA2_SRC_URL 'jinja2'
--- run_echo.sh
+++ run_echo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./tools/gn/linux64/gn gen out/Debug
+./buildtools/gn/linux64/gn gen out/Debug
 ninja -C out/Debug echo
 ./mojo/public/tools/download_shell_binary.py
 ./mojo/public/tools/prebuilt/mojo_shell --origin=file://`pwd`/out/Debug "mojo://echo_client"
