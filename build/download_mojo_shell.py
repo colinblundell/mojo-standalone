838e1771c77298458f1fc8c2f7468f5916a5c65f
Colin Blundell <blundell@chromium.org>
2014-11-04 10:53:04 +0100

    Bring in all of //mojo/public

--- build/download_mojo_shell.py
+++ build/download_mojo_shell.py
@@ -10,20 +10,18 @@ import tempfile
 import zipfile
 
 current_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(current_path, "..", "..", "..", "tools"))
-# pylint: disable=F0401
-import find_depot_tools
+root_path = os.path.join(current_path, "..")
 
 if not sys.platform.startswith("linux"):
   print "Not supported for your platform"
   sys.exit(0)
 
-version_path = os.path.join(current_path, "../VERSION")
+version_path = os.path.join(root_path, "mojo/public/VERSION")
 with open(version_path) as version_file:
   version = version_file.read().strip()
 
-prebuilt_file_path = os.path.join(current_path, "prebuilt")
-stamp_path = os.path.join(prebuilt_file_path, "VERSION")
+mojo_shell_path = os.path.join(root_path, "buildtools")
+stamp_path = os.path.join(mojo_shell_path, "MOJO_SHELL_VERSION")
 
 try:
   with open(stamp_path) as stamp_file:
@@ -38,17 +36,15 @@ basename = platform + ".zip"
 
 gs_path = "gs://mojo/shell/" + version + "/" + basename
 
-depot_tools_path = find_depot_tools.add_depot_tools_to_path()
-gsutil_exe = os.path.join(depot_tools_path, "third_party", "gsutil", "gsutil")
+gsutil_exe = os.path.join(root_path, "third_party", "gsutil", "gsutil")
 
 with tempfile.NamedTemporaryFile() as temp_zip_file:
-  subprocess.check_call([gsutil_exe, "--bypass_prodaccess",
-                         "cp", gs_path, temp_zip_file.name])
+  subprocess.check_call([gsutil_exe, "cp", gs_path, temp_zip_file.name])
   with zipfile.ZipFile(temp_zip_file.name) as z:
     zi = z.getinfo("mojo_shell")
     mode = zi.external_attr >> 16L
-    z.extract(zi, prebuilt_file_path)
-    os.chmod(os.path.join(prebuilt_file_path, "mojo_shell"), mode)
+    z.extract(zi, mojo_shell_path)
+    os.chmod(os.path.join(mojo_shell_path, "mojo_shell"), mode)
 
 with open(stamp_path, 'w') as stamp_file:
   stamp_file.write(version + "\n")
