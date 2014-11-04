349c9fe5e3dc7b626a8f1733c81e059c6e4ff9d0
Colin Blundell <blundell@chromium.org>
2014-11-04 12:28:24 +0100

    Make script more usable

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+example_name=$1
+if [[ $example_name == "echo" ]]
+then
+  target_name="echo"
+  library_name="echo_client"
+elif [[ $example_name == "apptest" ]]
+then
+  target_name="apptest"
+  library_name="example_apptests"
+else
+  echo "Unknown example: $example_name"
+  exit 1
+fi
+
 ./buildtools/gn gen out/Debug
-./buildtools/ninja -C out/Debug $1
-./buildtools/mojo_shell --origin=file://`pwd`/out/Debug "mojo://$2"
+./buildtools/ninja -C out/Debug $target_name
+./buildtools/mojo_shell --origin=file://`pwd`/out/Debug "mojo://$library_name"
