19c556d679611383c55718b80e1701400b12a383
Colin Blundell <blundell@chromium.org>
2014-11-04 10:05:55 +0100

    Eliminate last reliance on depot_tools

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -33,8 +33,15 @@ function install_dep_from_tarfile {
 
 # BODY
 
+# Install gsutil.
+cd $THIRD_PARTY_DIR
+curl --remote-name https://storage.googleapis.com/pub/gsutil.tar.gz
+tar xfz gsutil.tar.gz
+rm gsutil.tar.gz
+
 # Install gn.
-~/depot_tools/download_from_google_storage -f --bucket chromium-gn --output $ROOT_DIR/buildtools/gn 56e78e1927e12e5c122631b7f5a46768e527f1d2
+$THIRD_PARTY_DIR/gsutil/gsutil cp gs://chromium-gn/56e78e1927e12e5c122631b7f5a46768e527f1d2 $BUILDTOOLS_DIR/gn
+chmod 700 $BUILDTOOLS_DIR/gn
 
 # Build and install ninja.
 cd $THIRD_PARTY_DIR
@@ -42,6 +49,7 @@ rm -rf ninja
 git clone https://github.com/martine/ninja.git -b v1.5.1
 ./ninja/bootstrap.py
 cp ./ninja/ninja $BUILDTOOLS_DIR
+chmod 700 $BUILDTOOLS_DIR/ninja
 
 # Install gtest at the correct revision.
 rm -rf $GTEST_DIR
@@ -49,12 +57,6 @@ mkdir -p $GTEST_DIR
 git clone https://chromium.googlesource.com/external/googletest.git $GTEST_DIR
 cd $GTEST_DIR
 git checkout 4650552ff637bb44ecf7784060091cbed3252211 # from svn revision 692
-
-# Install gsutil.
-cd $THIRD_PARTY_DIR
-curl --remote-name https://storage.googleapis.com/pub/gsutil.tar.gz
-tar xfz gsutil.tar.gz
-rm gsutil.tar.gz
 
 # Download and extract PLY.
 # Homepage:
