f58ba62a15a6b2f3ad1e192f49142f5d5adb3be6
Colin Blundell <blundell@chromium.org>
2014-11-04 09:28:44 +0100

    Alter install-build-deps script

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -2,6 +2,7 @@
 
 ROOT_DIR="$(dirname $(realpath $(dirname "${BASH_SOURCE[0]}")))"
 BUILDTOOLS_DIR="$ROOT_DIR/buildtools"
+THIRD_PARTY_DIR="$ROOT_DIR/third_party/"
 GTEST_DIR="$ROOT_DIR/testing/gtest"
 
 function install_dep_from_tarfile {
@@ -10,10 +11,9 @@ function install_dep_from_tarfile {
   FILENAME="$(basename $SRC_URL)"
 
   BUILD_DIR="$(basename $FILENAME .tar.gz)"
-  THIRD_PARTY="$ROOT_DIR/third_party/"
-  INSTALL_DIR="$THIRD_PARTY/$PACKAGE_DIR"
+  INSTALL_DIR="$THIRD_PARTY_DIR/$PACKAGE_DIR"
   OUT_DIR="$INSTALL_DIR/$BUILD_DIR/$PACKAGE_DIR"
-  OLD_DIR="$THIRD_PARTY/$PACKAGE_DIR.old"
+  OLD_DIR="$THIRD_PARTY_DIR/$PACKAGE_DIR.old"
 
   mkdir -p "$INSTALL_DIR"
   cd "$INSTALL_DIR"
@@ -34,16 +34,17 @@ function install_dep_from_tarfile {
 # BODY
 
 # Install gn.
-~/depot_tools/download_from_google_storage --bucket chromium-gn --output $ROOT_DIR/buildtools/gn 56e78e1927e12e5c122631b7f5a46768e527f1d2
+~/depot_tools/download_from_google_storage -f --bucket chromium-gn --output $ROOT_DIR/buildtools/gn 56e78e1927e12e5c122631b7f5a46768e527f1d2
 
 # Build and install ninja.
-mkdir -p $BUILDTOOLS_DIR/src
-cd $BUILDTOOLS_DIR/src
+cd $THIRD_PARTY_DIR
+rm -rf ninja
 git clone https://github.com/martine/ninja.git -b v1.5.1
 ./ninja/bootstrap.py
 cp ./ninja/ninja $BUILDTOOLS_DIR
 
 # Install gtest at the correct revision.
+rm -rf $GTEST_DIR
 mkdir -p $GTEST_DIR
 git clone https://chromium.googlesource.com/external/googletest.git $GTEST_DIR
 cd $GTEST_DIR
@@ -51,12 +52,12 @@ git checkout 4650552ff637bb44ecf7784060091cbed3252211 # from svn revision 692
 
 cd $ROOT_DIR
 
-# Download and extract PLY
+# Download and extract PLY.
 # Homepage:
 # http://dabeaz.com/ply
 install_dep_from_tarfile "http://dabeaz.com/ply/ply-3.4.tar.gz" "ply"
 
-# Download and extract Jinja2
+# Download and extract Jinja2.
 # Homepage:
 # http://jinja.pocoo.org/
 # Installation instructions:
@@ -67,7 +68,7 @@ JINJA2_SRC_URL="https://pypi.python.org/packages/source/"
 JINJA2_SRC_URL+="J/Jinja2/Jinja2-2.7.1.tar.gz"
 install_dep_from_tarfile $JINJA2_SRC_URL 'jinja2'
 
-# Download and extract MarkupSafe
+# Download and extract MarkupSafe.
 # Homepage:
 # https://pypi.python.org/pypi/MarkupSafe
 MARKUPSAFE_SRC_URL="https://pypi.python.org/packages/source/"
